7671609b3b651343db409b34a918e3154881e123
Joarkemu <joarkemu@gmail.com>
2024-08-27 15:55:32 -0500

    commmit implemantar Urls

--- backendIG/backendIG/urls.py
+++ backendIG/backendIG/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
+from rest_framework.routers import DefaultRouter
+from apiIG.views import ProductoViewSet, inventario_list, entregar_inventario
+
+router = DefaultRouter
+router.register(r'productos', ProductoViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('apiIG/', include(router.urls)),
+    path('apiIG/inventario/', inventario_list),
+    path('apiIG/inventario/<str:id>/entregar/', entregar_inventario),
 ]
